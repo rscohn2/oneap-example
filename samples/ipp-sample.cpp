c2f0d41dc511af132d9dcb08a5003e80f623395a
Preeti Suman <47010227+psuman65@users.noreply.github.com>
2021-03-09 17:58:08 -0600

    sample for IPP (#4)

---
+++ samples/ipp-sample.cpp
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2020 Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+
+#include <iostream>
+
+#include "ippcore.h"
+
+int main() {
+    const IppLibraryVersion *lib;
+    
+    /* Get IPP library version info */
+    lib = ippGetLibVersion();
+    std::cout << lib->Name << " " << lib->Version << std::endl;
+    
+    return 0;
+}
