b7e3086c9eba33e04a098622a09559df299d024b
Preeti Suman <47010227+psuman65@users.noreply.github.com>
2021-03-11 17:34:49 -0600

    added sample tests for IPPCP and VPL (#5)

---
+++ samples/ippcp-sample.cpp
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2020 Intel Corporation
+//
+// SPDX-License-Identifier: MIT
+
+#include <iostream>
+
+#include "ippcp.h"
+
+int main()
+{
+    const IppLibraryVersion *lib;
+    
+    /* Get IPP Cryptography library version info */
+    lib = ippcpGetLibVersion();
+    std::cout << lib->Name << " " << lib->Version << std::endl;
+    
+    return 0;
+}
