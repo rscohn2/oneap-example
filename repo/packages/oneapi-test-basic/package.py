e43ed26be90728338624617ee7361050e78f8edc
Robert Cohn <robert.s.cohn@intel.com>
2023-11-21 12:58:06 -0600

    better informational messages

--- repo/packages/oneapi-test-basic/package.py
+++ repo/packages/oneapi-test-basic/package.py
@@ -5,6 +5,7 @@
 
 # flake8: noqa
 
+from llnl.util import tty
 from spack import *
 
 
@@ -79,6 +80,15 @@ class OneapiTestBasic(Package):
         targets = []
         for c in OneapiTestBasic.samples:
             if '+all' in self.spec or f'+{c}' in self.spec:
+                component_spec = self.spec[f'intel-oneapi-{c}']
+                tty.debug(
+                    (
+                        f'{c}\n'
+                        f' include directories: {component_spec.headers.directories}\n'
+                        f' libraries: {component_spec.libs}\n'
+                        f' library directories: {component_spec.libs.directories}\n'
+                    )
+                )
                 if c == 'mpi':
                     targets.append(
                         f'MPI_PREFIX={self.spec["mpi"].prefix}/mpi/latest'
